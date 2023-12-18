4cabb3821f59e4f845c215bf9a670533af9fcbd7
itllsendamsg <marcin.chudy.office@gmail.com>
2023-12-18 03:28:57 +0100

    18 12 23 ENHKX Med+) ) ) Drone "Action.java" & "Answer.java" reformatted correctly as a part of the project files cleaning process.

--- src/main/java/pl/stormit/ideas/Action.java
+++ src/main/java/pl/stormit/ideas/Action.java
@@ -3,22 +3,23 @@ package pl.stormit.ideas;
 import java.util.Objects;
 
 public enum Action {
-LIST("list"), ADD("add");
+    LIST("list"), ADD("add");
 
-private final String value;
+    private final String value;
+
     Action(String value) {
         this.value = value;
     }
 
-    public static Action of(String value){
-        for(Action action : values()){
-            if (Objects.equals(action.value,value)){
+    public static Action of(String value) {
+        for (Action action : values()) {
+            if (Objects.equals(action.value, value)) {
                 return action;
             }
         }
 
         throw new IllegalArgumentException("\n" +
-                "\n"+
+                "\n" +
                 "       ----------------------------------------------------------------------------------- \n" +
                 "       *********************************************************************************** \n" +
                 "       ----------------------------------------------------------------------------------- \n" +
--- src/main/java/pl/stormit/ideas/model/Answer.java
+++ src/main/java/pl/stormit/ideas/model/Answer.java
@@ -1,19 +1,23 @@
 package pl.stormit.ideas.model;
 
 public class Answer {
-private String name;
+    private String name;
 
     public Answer() {
     }
+
     public Answer(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     @Override
     public String toString() {
         return "Answer{" +
